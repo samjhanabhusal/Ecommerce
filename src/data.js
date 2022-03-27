05b4535bf22cfd218e61e895d94114563244eb3a
samjhanabhusal <bhusalsam131340@gmail.com>
2022-03-27 13:23:08 +0545

    second commit

---
+++ src/data.js
@@ -0,0 +1,27 @@
+export const sliderItems = [
+    {
+      id: 1,
+      // img: "https://source.unsplash.com/600*900/?clothes, autumn",
+      img:"https://media.istockphoto.com/photos/womens-summer-straw-hat-wicker-bag-white-sundress-sunglasses-and-on-picture-id1220167951?s=612x612",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+
+      // img: "https://source.unsplash.com/600*900",
+      img:"https://images.unsplash.com/photo-1603197788269-c76bbc23b1de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2196&q=80",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img:"https://images.unsplash.com/photo-1503342217505-b0a15ec3261c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+ 
